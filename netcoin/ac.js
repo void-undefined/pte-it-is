45b8739630f1832736c72236902be4b45fa66457
VoidUndefined <mephisto05@mail.ru>
2024-07-27 00:50:12 +0300

    Netcoin

--- netcoin/ac.js
+++ netcoin/ac.js
@@ -2,7 +2,7 @@
 // @name        Netcoin Autoclicker
 // @match       *://netcoin.layernet.ai/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      void_undefined
 // ==/UserScript==
 
